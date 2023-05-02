6acf008490c826305fd69608ab16336b421c167d
Kevin Loquencio <kevin.loquencio@gmail.com>
2023-05-02 14:12:19 +0800

    Update commands and files

--- app/deploy.sh
+++ app/deploy.sh
@@ -3,6 +3,8 @@ mkdir ~/public_html
 cp -r ~/repositories/poxel-graphics/app/* ~/public_html
 cp ~/env/.env ~/public_html/.env
 cd ~/public_html
+# Install
+composer install
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
--- app/shared.sh
+++ app/shared.sh
@@ -1,3 +1,3 @@
 cp -r ./public/ ./
-sed -i.php "s/\/..\//.\//g" index.php
+sed -i.php "s/\/..\//\//g" index.php
 rm index.php.php
