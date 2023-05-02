276f0e2ee1a1834ef7dac76de2e853cd21fbfaa2
Kevin Loquencio <kevin.loquencio@gmail.com>
2023-05-02 15:25:33 +0800

    improve deploy commands

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,10 +1,10 @@
 rm -rfd ~/public_html
 mkdir ~/public_html
-cp -a ~/repositories/poxel-graphics/app/. ~/public_html
+cp -r ~/repositories/poxel-graphics/app/. ~/public_html
 cp ~/env/.env ~/public_html/.env
 cd ~/public_html
 # Install
-composer install
+composer install --optimize-autoloader --no-dev
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
